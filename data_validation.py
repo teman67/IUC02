bcdfba2f3dd6d830238652fc98a1036c8a8fa120
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-02-06 14:42:21 +0100

    fix the style of data report on data_validation page

--- data_validation.py
+++ data_validation.py
@@ -8,10 +8,44 @@ import sys
 
 
 def data_validation_page():
-    st.title("RDF SHACL Validator")
-    st.write("This page allows you to validate your data against a SHACL shape.")
-    st.write("Please upload your data and SHACL shape files below.")
 
+    # Set the page background image
+    page_bg_img = '''
+    <style>
+    [data-testid="stApp"] {
+        background-image: url("https://owncloud.fraunhofer.de/index.php/s/O8IEa05wALHXyVh/download?path=%2FLogos%20und%20Grafiken%2FLogo_NFDI-MatWerk%2F2020-07-03_neuesLogo&files=Logo_NFDI-MatWerk-1000px.png");
+        background-position: 95% 95%;
+        background-size: 20vh;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+
+    </style>
+    '''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+    st.markdown("""
+        <style>
+            div[data-testid="stFileUploader"] {
+                width: 600px !important;
+                padding: 5px !important;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+
+    st.markdown('''
+    <h1 style=" text-align: left; margin-bottom: 10px">
+        RDF SHACL Validator
+    </h1>
+    <h3 style=" text-align: left; margin-top: 0px; margin-bottom: 2px">
+        This page allows you to validate your data against a SHACL shape.
+    </h3>
+    <h4 style=" text-align: left; margin-top: 0px; margin-bottom: 30px">
+        Please upload your data and SHACL shape files below.
+    </h4>
+    ''', unsafe_allow_html=True)
+    
     
     rdf_file = st.file_uploader("Upload RDF (Turtle format)", type=["ttl"])
     shacl_file = st.file_uploader("Upload SHACL (Turtle format)", type=["ttl"])
