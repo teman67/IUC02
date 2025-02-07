265498d7281bdd81f9fe25427c5fca58f1a93d77
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-02-07 09:46:08 +0100

    add background image to data validation page

--- data_validation.py
+++ data_validation.py
@@ -2,7 +2,22 @@ import streamlit as st
 import pyshacl
 from pyshacl import validate
 from rdflib import Graph
+import base64
+import os
 
+def get_base64_encoded_image(image_path):
+    """Convert image to Base64."""
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+# Use absolute path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(current_dir, "Nfdi_Matwer_logo.png")
+
+if os.path.exists(image_path):
+    base64_img = get_base64_encoded_image(image_path)
+else:
+    st.error(f"Image not found: {image_path}")
 
 def get_example_file(file_path):
     """Reads and returns the content of an example file."""
@@ -11,6 +26,20 @@ def get_example_file(file_path):
 
 
 def data_validation_page():
+    # Set the page background image
+    page_bg_img = f'''
+        <style>
+        [data-testid="stApp"] {{
+            background-color: white;
+            background-image: url("data:image/png;base64,{base64_img}");
+            background-position: 95% 95%;
+            background-size: 20vh;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        </style>
+        '''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
     st.markdown("""
         <style>
             div[data-testid="stFileUploader"] {
@@ -24,9 +53,9 @@ def data_validation_page():
     """, unsafe_allow_html=True)
 
     st.markdown('''
-    <h1 style=" text-align: left; margin-bottom: 10px">
+    <h2 style=" text-align: left; margin-bottom: 5px">
         SHACL Validator
-    </h1>
+    </h2>
     <h3 style=" text-align: left; margin-top: 0px; margin-bottom: 2px">
         This page allows you to validate your data against a SHACL shape.
     </h3>
