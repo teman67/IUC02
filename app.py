12e436b9a0acaf733d377b32cb8bcee0a6113365
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-02-07 09:54:25 +0100

    add data generation page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="IUC02", page_icon="🌐", layout="wide")
 # load pages scripts
 from page_summary import page_summary_body
 from data_validation import data_validation_page
-# from app_pages.page_LLM import *
+from data_generation import data_generation_page
 # from app_pages.history import *
 # from app_pages.page_json_viewer import *
 #from app_pages.graph import graph_visualizer_page
@@ -16,8 +16,8 @@ app = MultiPage(app_name="IUC02")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("IUC02 Overview", page_summary_body)
+app.add_page("Data Generation",data_generation_page)
 app.add_page("Data Validation", data_validation_page)
-# app.add_page("Explore Ollama Models", LLM_models)
 # app.add_page("Chat History Overview", display_conversation_history)
 # app.add_page("JSON File Viewer", json_viewer)
 # #app.add_page("Graph Visualizer", graph_visualizer_page)
