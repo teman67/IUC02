32a7d8b6a63d0b4aa6ad2a2f3ebde5da941e3291
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-02-07 13:45:27 +0100

    add downladable files to data generation page

--- data_generation.py
+++ data_generation.py
@@ -65,9 +65,9 @@ def data_generation_page():
         st.markdown(
             f'''
             <div style="padding:10px; border:2px solid black; text-align:center; background-color:#FFCDD2; border-radius:10px;">
-                <b>Extra Step</b><br>
-                <a href="{ext_file1}" download style="color:blue; text-decoration:none;">File 1</a><br>
-                <a href="{ext_file2}" download style="color:blue; text-decoration:none;">File 2</a>
+                <b>Mapping Step</b><br>
+                <a href="{ext_file2}" download style="color:blue; text-decoration:none;">Metadata Schema</a><br>
+                <a href="{ext_file1}" download style="color:blue; text-decoration:none;">Mapping Document</a>
             </div>
             ''',
             unsafe_allow_html=True
@@ -83,7 +83,7 @@ def data_generation_page():
         st.markdown(
             f'''
             <div style="padding:20px; border:2px solid black; text-align:center; background-color:#E3F2FD; border-radius:10px;">
-                <b>Step 1</b><br>
+                <b>Creep experiment input file (.lis file)</b><br>
                 <a href="{file1}" download style="color:blue; text-decoration:none;">Download</a>
             </div>
             ''',
@@ -91,14 +91,14 @@ def data_generation_page():
         )
 
     with col2:
-        st.markdown('<div style="text-align:center; font-size:24px;">↓</div>', unsafe_allow_html=True)  # Down arrow
-        st.markdown('<div style="text-align:center; font-size:24px;">→</div>', unsafe_allow_html=True)  # Right arrow
+        st.markdown('<div style="text-align:center; font-size:30px;">↓</div>', unsafe_allow_html=True)  # Down arrow
+        st.markdown('<div style="text-align:center; font-size:40px;">→</div>', unsafe_allow_html=True)  # Right arrow
 
     with col3:
         st.markdown(
             f'''
             <div style="padding:20px; border:2px solid black; text-align:center; background-color:#BBDEFB; border-radius:10px;">
-                <b>Step 2</b><br>
+                <b>Populated Schema</b><br>
                 <a href="{file2}" download style="color:blue; text-decoration:none;">Download</a>
             </div>
             ''',
@@ -106,28 +106,28 @@ def data_generation_page():
         )
 
     with col4:
-        st.markdown('<div style="text-align:center; font-size:24px;">→</div>', unsafe_allow_html=True)
+        st.markdown('<div style="text-align:center; font-size:40px;">→</div>', unsafe_allow_html=True)
 
     with col5:
         st.markdown(
             f'''
             <div style="padding:20px; border:2px solid black; text-align:center; background-color:#90CAF9; border-radius:10px;">
                 <b>Step 3</b><br>
-                <a href="{file3a}" download style="color:blue; text-decoration:none;">File A</a><br>
-                <a href="{file3b}" download style="color:blue; text-decoration:none;">File B</a>
+                <a href="{file3a}" download style="color:blue; text-decoration:none;">Data Graph</a><br>
+                <a href="{file3b}" download style="color:blue; text-decoration:none;">Shape Graph</a>
             </div>
             ''',
             unsafe_allow_html=True
         )
 
     with col6:
-        st.markdown('<div style="text-align:center; font-size:24px;">→</div>', unsafe_allow_html=True)
+        st.markdown('<div style="text-align:center; font-size:40px;">→</div>', unsafe_allow_html=True)
 
     with col7:
         st.markdown(
             f'''
             <div style="padding:20px; border:2px solid black; text-align:center; background-color:#64B5F6; border-radius:10px;">
-                <b>Step 4</b>
+                <b>Validation Protocol</b>
             </div>
             ''',
             unsafe_allow_html=True
